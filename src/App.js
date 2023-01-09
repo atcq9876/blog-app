614c762c4b30d98cbff1041c1c9a6143c821bce3
Andy <ajscottlewis@gmail.com>
2023-01-09 17:50:22 +0000

    Add home and navbar components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const testString = "Test dynamic variable"
-  
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
-        <p>{ testString }</p>
+        <Home />
       </div>
     </div>
   );
