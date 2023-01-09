5b466bb983eb8c65e1fe6e8566b6b290a1a6a311
Andy <ajscottlewis@gmail.com>
2023-01-09 19:46:39 +0000

    Add event listening buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('Hello');
+    console.log(e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('Hello ' + name);
+    console.log(e.target);
+  }
+  
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => {handleClickAgain('Andy', e)}}>Click me too</button>
     </div>
   );
 }
