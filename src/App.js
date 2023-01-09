239a1c230e905729d2ad9d8698f214596e952118
Andy <ajscottlewis@gmail.com>
2023-01-09 17:12:14 +0000

    Add a div containing a dynamic variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
 function App() {
+  const testString = "Test dynamic variable"
+  
   return (
     <div className="App">
       <div className="content">
         <h1>App Component</h1>
+        <p>{ testString }</p>
       </div>
     </div>
   );
