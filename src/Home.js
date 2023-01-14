cd21df3fd3f86eef48340658efed6fa9f670b2f1
Andy <ajscottlewis@gmail.com>
2023-01-14 18:06:18 +0000

    Add loading and error messages for blogs fetch request

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,33 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-      .then(response => response.json())
-      .then(data => setBlogs(data));
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Failed to fetch data");
+        }
+        response.json();
+        })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null)
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      });
   }, []);
   
   return (
     <div className="home">
+      {isPending && <div className="loading">Loading blogs...</div>}
+      {isError && <div className="error">{ error }</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
