c9b0916b9aea960824895170e7764c8420d056fd
Andy <ajscottlewis@gmail.com>
2023-01-15 17:40:52 +0000

    Add form to create blog page, add basic handleSubmit function

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title: </label>
+        <input type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body: </label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author: </label>
+        <select
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 }
